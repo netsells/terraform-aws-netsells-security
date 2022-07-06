65ef1aeeb3ed553cd509f1e267fa7cfad1127380
Sam Jordan <sam@netsells.co.uk>
2022-07-06 09:50:52 +0100

    Fixes xml path

--- iam-google.tf
+++ iam-google.tf
@@ -1,7 +1,7 @@
 # Setup identity provider to allow Google login
 resource "aws_iam_saml_provider" "default" {
   name                   = "NetsellsGoogleWorkspace"
-  saml_metadata_document = file("files/GoogleIDPMetadata.xml")
+  saml_metadata_document = file("${path.module}/files/GoogleIDPMetadata.xml")
 }
 
 data "aws_iam_policy_document" "saml_trust_policy" {
